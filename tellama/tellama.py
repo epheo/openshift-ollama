2aa678b23edd51d7a37949258c109d9dab8e46ca
epheo <root@epheo.eu>
2024-02-06 13:02:15 +0100

    refactor: improve readability

--- tellama/tellama.py
+++ tellama/tellama.py
@@ -1,47 +1,17 @@
 #!/usr/bin/env python
 # pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
 
 import os
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OLLAMA_ENDPOINT = os.getenv("OLLAMA_ENDPOINT")
 
-import logging
 import requests
 
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import Update
+from telegram.ext import Application, ContextTypes, MessageHandler, filters
 
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-# set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
-
-logger = logging.getLogger(__name__)
-
-
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
-
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    # await update.message.reply_text(update.message.text)
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     data = {
       "model": "mistral:7b",
       "messages": [
@@ -52,10 +22,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
       ],
       "stream": False
     }
-
-    # Send the user's message to your Ollama-hosted LLM and get the response
     response = requests.post(OLLAMA_ENDPOINT, json=data )
-
 
     if response.status_code == 200:
         ollama_response = response.json().get('message').get('content')
@@ -63,22 +30,10 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     else:
         await update.message.reply_text('Sorry, there was an error processing your request.')
 
-
 def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token(TELEGRAM_TOKEN).build()
-
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-
 
 if __name__ == "__main__":
     main()
